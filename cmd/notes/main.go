a9e8c96506d1bf54e2872f21cb65023f889c23f7
Baptiste Mayelle <baptiste.mayelle@traefik.io>
2023-08-09 11:16:54 +0200

    add server

--- cmd/notes/main.go
+++ cmd/notes/main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	pb "github.com/bbengfort/notes/v1"
+	"google.golang.org/grpc"
+)
+
+type NoteServer struct {
+	pb.UnimplementedNoteServiceServer
+}
+
+func (n NoteServer) Fetch(ctx context.Context, filter *pb.NoteFilter) (*pb.Notebook, error) {
+	return &pb.Notebook{
+		Error: nil,
+		Notes: []*pb.Note{
+			{
+				Id:        1,
+				Timestamp: "1",
+				Author:    "Toto",
+				Text:      "Pouet",
+				Private:   false,
+			},
+			{
+				Id:        2,
+				Timestamp: "2",
+				Author:    "Toto",
+				Text:      "Pouet2",
+				Private:   false,
+			},
+		},
+	}, nil
+}
+
+func (n NoteServer) Create(ctx context.Context, note *pb.Note) (*pb.Notebook, error) {
+	return &pb.Notebook{
+		Error: nil,
+		Notes: []*pb.Note{note},
+	}, nil
+}
 
 func main() {
-	fmt.Printf("TODO: create server")
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:8080"))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+	pb.RegisterNoteServiceServer(grpcServer, &NoteServer{})
+	grpcServer.Serve(lis)
 }
